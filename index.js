5bb5147dd7c2654427bb12d65ab86cc3574e336e
VicktorS98 <100975523+VicktorS98@users.noreply.github.com>
2022-06-22 13:39:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ $(".toggle").click(function() {
   $(".logo-toggle-1").hide("slow");
   $(".logo-nav-abierto").css("filter", "invert(100%) sepia(100%) saturate(0%) hue-rotate(164deg) brightness(102%) contrast(101%)")
   $("nav").toggleClass("active");
+  $("html, body").css({
+    overflow: "hidden",
+    height: "100%"
+  });
   if (!$("nav").hasClass("active")) {
     $(".logo-toggle-1").show("slow");
+    $("html, body").css({
+      overflow: "auto",
+      height: "auto"
+    });
   }
 });
+
 
 $(document).ready(function() {
   $(".tab-1").css("border-bottom", "5px solid rgb(83, 99, 238)").css("color", "rgb(83, 99, 238)");
